4647390464d7214cd512e8d7426c2d50d036ff8f
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2019-02-24 09:03:52 +0200

    Add pager

--- app/src/main/java/agilefreaks/com/viewpage2demo/CheesePagerAdapter.kt
+++ app/src/main/java/agilefreaks/com/viewpage2demo/CheesePagerAdapter.kt
@@ -1,2 +1,22 @@
 package agilefreaks.com.viewpage2demo
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CheesePagerAdapter(private val cheeses: Array<String>): RecyclerView.Adapter<CheesePagerAdapter.CheeseViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CheeseViewHolder =
+        CheeseViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.cheese_list_item, parent, false))
+
+    override fun getItemCount(): Int = cheeses.size
+
+    override fun onBindViewHolder(holder: CheeseViewHolder, position: Int) {
+        holder.cheeseName.text = cheeses[position]
+    }
+
+    class CheeseViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val cheeseName: TextView = view.findViewById(R.id.cheese_name)
+    }
+}
--- app/src/main/java/agilefreaks/com/viewpage2demo/MainActivity.kt
+++ app/src/main/java/agilefreaks/com/viewpage2demo/MainActivity.kt
@@ -2,11 +2,17 @@ package agilefreaks.com.viewpage2demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cheeses = arrayOf("Abbaye de Timadeuc", "Baladi", "Paesanella Caciotta")
+        view_pager.adapter = CheesePagerAdapter(cheeses)
+        view_pager.orientation = ViewPager2.ORIENTATION_VERTICAL
     }
 }
